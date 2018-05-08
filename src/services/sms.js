bb2272453d13b413bf4c226b9350640d26b7eebd
zuozuo <zuoyonghui@baidu.com>
2018-05-08 17:08:46 +0800

    format sms message

--- src/services/sms.js
+++ src/services/sms.js
@@ -7,7 +7,7 @@ async function deliver(message) {
   return Notifier.deliver(message, 'email', () => {
     const smsClient = smsPool.acquire();
     return smsClient.then(client => {
-      let content = `${process.env.PHONES}@${message.title}, ${message.content}\r\n`;
+      let content = `${process.env.PHONES}@${message.title}\r\n${message.content}\r\n`;
       client.write(content);
     })
   })
