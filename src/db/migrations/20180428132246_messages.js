3be000d7c5be43e9424b4347af4475a95d1770fa
zuozuo <zuoyonghui@baidu.com>
2018-05-08 17:31:06 +0800

    add pm2 process.yml

--- src/db/migrations/20180428132246_messages.js
+++ src/db/migrations/20180428132246_messages.js
@@ -2,12 +2,12 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('messages', (table) => {
     table.increments();
     table.string('level');
+    table.string('targets');
     table.text('content');
     table.text('response');
     table.text('description');
     table.string('title').notNullable();
     table.string('source').notNullable();
-    table.string('targets').notNullable();
     table.integer('retries').defaultTo('3');
     table.integer('attampts').defaultTo('0');
     table.boolean('success').defaultTo('false');
--- src/index.js
+++ src/index.js
@@ -5,13 +5,12 @@ const bodyParser = require('koa-bodyparser');
 const messageRoutes = require('./routes/messages');
 
 const app = new Koa();
-const PORT = 3000;
 
 app.use(bodyParser());
 app.use(messageRoutes.routes());
 
-const server = app.listen(PORT, () => {
-  console.log(`Server listening on port: ${PORT}`);
+const server = app.listen(process.env.SERVICE_PORT, () => {
+  console.log(`Server listening on port: ${process.env.SERVICE_PORT}`);
 });
 
 module.exports = server;
